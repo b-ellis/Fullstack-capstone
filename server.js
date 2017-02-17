57f389f785211eed110b8a21db011f779790be5e
b-ellis <bradjellis18@gmail.com>
2017-02-17 09:43:02 -0800

    server changes

--- server.js
+++ server.js
@@ -9,8 +9,10 @@ import events from 'events'
 
 import config from './config'; 
 import User from './models/user';
+import Profile from './models/profile';
 import { register } from './server/register';
 import { strategy } from './server/validation';
+import { getApi } from './server/api';
 
 const jsonParser = bodyParser.json();
 const app = express();
@@ -52,30 +54,23 @@ app.post('/register', (req, res) => {
 passport.use(strategy);
 app.use(passport.initialize());
 
-const getApi = (endpoint) => {
-	const emitter = new events.EventEmitter();
-	const Request = unirest.get('https://www.mysportsfeeds.com/api/feed/pull/nfl/' + endpoint);
-	Request.auth('brad', 'Bushnellcraft', true)
-	.end((res) => {
-		if(res.ok) {
-			emitter.emit('end', res.body);
-		}
-		else {
-			emitter.emit('error', res.code);
-		}
-	});
-	return emitter;
-}
-
 app.post('/login', passport.authenticate('basic', {
 													session: false,
-													successRedirect: '/',
+													successRedirect: '/results',
 				                                   failureRedirect: '/login',
 				                                   failureFlash: true 
 													}), (req, res) => {
 
 	const user = req.user;
 
+	const profile = new Profile({
+		name: user.firstName + ' ' + user.lastName,
+		username: user.userName,
+		email: user.email
+	});
+
+	res.json(profile);
+	console.log(profile);
 });
 
 app.get('/schedule', (req, res) => {
@@ -95,7 +90,7 @@ app.get('/schedule', (req, res) => {
 });
 
 app.get('/results', (req, res) => {
-	const endpoint = '22016-2017-regular/scoreboard.json?fordate=20160911&'
+	const endpoint = '2016-2017-regular/scoreboard.json?fordate=20160911&'
 	const result = getApi(endpoint);
 
 
