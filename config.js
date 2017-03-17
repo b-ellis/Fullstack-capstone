cb30863ea30cdd052afe8ea2f74c082d258c1961
b-ellis <bradjellis18@gmail.com>
2017-03-17 08:21:38 -0700

    switched to mlab

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-exports.DATABASE_URL = process.env.PROD_MONGODB ||
+exports.DATABASE_URL = 'mongodb://b-ellis:Marshall18@ds131890.mlab.com:31890/thinkfulcapstone' ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
                             'mongodb://localhost/capstone' :
